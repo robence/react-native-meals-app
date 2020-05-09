568d8190e459ef8cb1d1580d46b3a2e71612c5f5
rozsabence <robence@gmail.com>
2020-05-09 05:39:23 +0200

    render fallback text on favorites

--- src/screens/FavoritesScreen.jsx
+++ src/screens/FavoritesScreen.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { MealList } from '../components';
+import { View, StyleSheet } from 'react-native';
+import { MealList, DefaultText } from '../components';
 
 export default function FavoritesScreen() {
   const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
+  if (!favoriteMeals || favoriteMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favourite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList data={favoriteMeals} />;
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
